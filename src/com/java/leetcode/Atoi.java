8b8ab2b0b6166f78497b6c6308a47524d7e29904
apoorva_iyengar <apoorvakrisha@gmail.com>
2018-08-21 14:24:42 -0500

    comments and descriptions
    added comments.

--- src/com/java/leetcode/Atoi.java
+++ src/com/java/leetcode/Atoi.java
@@ -1,5 +1,21 @@
 package com.java.leetcode;
 
+import java.util.Scanner;
+
+/**
+ * @author apoorva
+ *
+ * The function first discards as many whitespace characters as necessary until the first non-whitespace character is found.
+ * Then, starting from this character, takes an optional initial plus or minus sign followed by as many numerical digits as possible,
+ * and interprets them as a numerical value. 
+ * The string can contain additional characters after those that form the integral number, 
+ * which are ignored and have no effect on the behavior of this function. 
+ * If the first sequence of non-whitespace characters in str is not a valid integral number, 
+ * or if no such sequence exists because either str is empty or it contains only whitespace characters, 
+ * no conversion is performed. 
+ * If no valid conversion could be performed, a zero value is returned.
+ */
+
 public class Atoi
 {
 	//"-4567"
@@ -46,7 +62,10 @@ public class Atoi
 	
 	public static void main(String[] args) 
 	{
-		System.out.println(convertToInteger("-4567"));
+		Scanner scanner = new Scanner(System.in);
+		
+		String in = scanner.next();
+		System.out.println(convertToInteger(in));
 	}
 
 }
